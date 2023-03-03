3414642ad5104505eb24446a010026609f97c038
Sergii Kozhushko <sergiikozhushko@gmail.com>
2023-03-03 11:26:19 +0100

    Задача очередь на основе двух стеков

---
+++ src/de/telran/java/egorov/QueueFromStacks.java
@@ -0,0 +1,62 @@
+/**
+ * TwoStack.java
+ *
+ * @author Sergii Kozhushko, sergiikozhushko@gmail.com
+ * Date of creation: 03-Mar-2023 10:21
+ */
+
+package de.telran.java.egorov;
+
+import java.util.Stack;
+
+// Задача: реализовать очередь, использовав только два стека внутри
+public class QueueFromStacks<T> {
+   private Stack<T> stack1 = new Stack();
+   private Stack<T> stack2 = new Stack();
+
+   // метод помещает элемент в очередь
+   public void push(T element){
+      stack1.push(element);
+      //return element
+   }
+
+   // извлекает-удаляет первый элемент очереди
+   public T pop(){
+      // если второй стек пуст, то перекидываем все элементы из стека1 в стек2
+      if (stack2.size() == 0) {
+         while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+         }
+      }
+      else {
+         // удаляем верхний элемент из стека2
+         return stack2.pop();
+      }
+      return null;
+   }
+   public void print(){
+      for (int i = stack2.size()-1; i >= 0; i--) {
+         System.out.print(stack2.get(i) + " ");
+      }
+      for (int i = 0; i < stack1.size() ; i++) {
+         System.out.print(stack1.get(i) + " ");
+      }
+   }
+
+   public static void main(String[] args) {
+      QueueFromStacks<String> queue = new QueueFromStacks<String>();
+      queue.pop();
+      queue.push("a1");
+      queue.push("a2");
+      queue.push("a3");
+      queue.push("a4");
+      queue.pop(); // удаляем a1
+      queue.push("a5");
+      queue.push("a6");
+      queue.push("a7");
+      queue.pop(); // удаляем второй
+      queue.push("a8");
+      queue.push("a9");
+      queue.print();
+   }
+}
--- src/de/telran/java/loshmanov/HomeWork11.java
+++ src/de/telran/java/loshmanov/HomeWork11.java
@@ -7,6 +7,15 @@
 
 package de.telran.java.loshmanov;
 
+// Дан массив nums целых чисел и целое число target,
+// найдите индексы двух элементов массива, чтобы сумма этих значений была равна target.
+// Предполагается, что существует всегда только одно решение и нельзя
+// использовать один и тот же элемент массива дважды.
+// Example 1: Input: nums = [2,7,11,15], target = 9
+// Output: [0,1] Explanation: Because nums[0] + nums[1] == 9,
+// we return [0, 1]. Example 2: Input: nums = [3,2,4], target = 6
+// Output: [1,2] Example 3: Input: nums = [3,3], target = 6 Output: [0,1]
+
 public class HomeWork11 {
     public final static int[] array = {1, 2, 10, 12, 7, 0, 2};
     public static int index1, index2;
